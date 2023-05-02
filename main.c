51afe8ce4a22ef52b0c1b8dc29b0896ebf633adc
MiguelParraBezon <130755567+MiguelParraBezon@users.noreply.github.com>
2023-05-02 18:33:36 +0200

    Update main.c

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main()
   setlocale(LC_NUMERIC, "spanish");
   
   FILE *pf;
-  pf = fopen("C:/Users/hecto/Downloads/ProyectoInforREE.csv", "r");
+  pf = fopen("ProyectoInforREE.csv", "r");
   
   if (pf == NULL)
   {
@@ -102,7 +102,7 @@ int main()
 	}
   }
   
-//  printf("Tipo %s %f ", generacion[0][0].tipo, generacion[0][0].magnitud);
+/*printf("Tipo %s %f ", generacion[0][0].tipo, generacion[0][0].magnitud);
   for(i=0; i<18; i++)
   {
 	printf("Tipo %s: ", generacion[i][0].tipo);
@@ -111,10 +111,212 @@ int main()
 		printf("%f ", generacion[i][j].magnitud);
 	}
   printf("\n");
-  }
+  }*/
 	
   fclose(pf);
   
+  int op, letra, ano;
+  do{
+  system("cls");
+  printf("ESTE PROGRAMA TIENE LA FUNCIÓN DE REALIZAR UN AMPLIO ESTUDIO ESTÁDISTICO CON RESPECTO \nA LOS VALORES RECOGIDOS POR LA RED ELÉCTRICA ESPAÑOLA ENTRE EL AÑO 2021 Y 2022. \n");
+  printf("Para comenzar con el estudio, por favor escoja entre las siguientes opciones:\n");
+  printf("1. Tipo de generación eléctrica.\n");
+  printf("2. Fecha de generación.\n");
+  printf("3. Salir.\n");
+  scanf("%d", &op);
+  system("cls");
   
+  switch(op){
+  	
+	case 1:
+	do{
+  		printf("¿Qué tipo de generación desea estudiar?\n");
+  		printf("1. Hidráulica\n");
+  		printf("2. Turbinación bombeo\n");
+  		printf("3. Nuclear\n");
+  		printf("4. Carbón\n");
+  		printf("5. Fuel+Gas\n");
+  		printf("6. Motores Diesel\n");
+  		printf("7. Turbnina de gas\n");
+  		printf("8. Turbina de vapor\n");
+  		printf("9. Ciclo combinado\n");
+  		printf("10. Hidroeólica\n");
+  		printf("11. Eólica\n");
+  		printf("12. Solar fotovoltaica\n");
+  		printf("13. Solar térmica\n");
+  		printf("14. Otras reonvavbles\n");
+  		printf("15. Cogeneración\n");
+  		printf("16. Residuos no renovables\n");
+  		printf("17. Residuos renovavles\n");
+  		printf("18. VOLVER ATRÁS\n");
+  		scanf("%d", &letra);
+  		
+  		switch(letra)
+		  	{
+  			case 1:
+  				break;
+  			case 2:
+  				break;
+  			case 3:
+  				break;
+  			case 4:
+  				break;
+  			case 5:
+  				break;
+  			case 6:
+  				break;
+  			case 7:
+  				break;
+  			case 8:
+  				break;
+  			case 9:
+  				break;
+  			case 10:
+  				break;
+  			case 11:
+  				break;
+  			case 12:
+  				break;
+  			case 13:
+  				break;
+  			case 14:
+  				break;
+  			case 15:
+  				break;
+  			case 16:
+  				break;
+  			case 17:
+  				break;
+  			case 18:
+  				break;
+  			default:
+  				system("cls");
+            	printf("Opción no válida. Por favor seleccione una opción válida.\n");
+            	break;
+  			}
+	}while(letra!=18);
+  		
+  	break;
+  
+  	case 2:
+  	do{
+	  
+  		printf("Escoja el año que quiera estudiar:\n");
+  		printf("1. 2021\n");
+  		printf("2. 2022\n");
+  		printf("3. VOLVER ATRÁS\n");
+  		scanf("%d", &ano);
+  		system("cls");
+		  
+		  switch(ano){
+  			
+			case 1:
+				printf("Ahora escoja el mes en el que desea realizar el estudio\n");
+				printf("1. Enero\n");
+				printf("2. Febrero\n");
+				printf("3. Marzo\n");
+				printf("4. Abril\n");
+				printf("5. Mayo\n");
+				printf("6. Junio\n");
+				printf("7. Julio\n");
+				printf("8. Agosto\n");
+				printf("9. Septiembre\n");
+				printf("10. Octubre\n");
+				printf("11. Noviembre\n");
+				printf("12. Diciembre\n");
+				int mes;
+				scanf("%d", &mes);
+				
+				switch(mes){
+		  			case 1:
+		  				break;
+		  			case 2:
+		  				break;
+		  			case 3:
+		  				break;
+		  			case 4:
+		  				break;
+		  			case 5:
+		  				break;
+		  			case 6:
+		  				break;
+		  			case 7:
+		  				break;
+		  			case 8:
+		  				break;
+		  			case 9:
+		  				break;
+		  			case 10:
+		  				break;
+		  			case 11:
+		  				break;
+		  			case 12:
+		  				break;
+					}
+			break;
+  			
+			case 2:
+				printf("Ahora escoja el mes en el que desea realizar el estudio\n");
+				printf("1. Enero\n");
+				printf("2. Febrero\n");
+				printf("3. Marzo\n");
+				printf("4. Abril\n");
+				printf("5. Mayo\n");
+				printf("6. Junio\n");
+				printf("7. Julio\n");
+				printf("8. Agosto\n");
+				printf("9. Septiembre\n");
+				printf("10. Octubre\n");
+				printf("11. Noviembre\n");
+				printf("12. Diciembre\n");
+				int mes2;
+				scanf("%d", &mes2);
+				
+				switch(mes2){
+		  			case 1:
+		  				break;
+		  			case 2:
+		  				break;
+		  			case 3:
+		  				break;
+		  			case 4:
+		  				break;
+		  			case 5:
+		  				break;
+		  			case 6:
+		  				break;
+		  			case 7:
+		  				break;
+		  			case 8:
+		  				break;
+		  			case 9:
+		  				break;
+		  			case 10:
+		  				break;
+		  			case 11:
+		  				break;
+		  			case 12:
+		  				break;
+					}	
+			case 3:
+				break;
+			default:
+				system("cls");
+            	printf("Opción no válida. Por favor seleccione una opción válida.\n");
+            	break;
+  	}
+    }while(ano!=3);
+  	break;
+	default:
+		system("cls");
+        printf("Opción no válida. Por favor seleccione una opción válida.\n");
+    	break;
+        
+	}
+	
+	}while(op!=3);
+
   return 0;
- }
+}
+
+
