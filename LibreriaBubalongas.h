e5b9095d6d1eded5e2b19ed4eaffe317c96ba16f
HectorGordo <hector.gordo@alumnos.upm.es>
2023-05-18 19:35:51 +0200

    Creación del proyecto

---
+++ LibreriaBubalongas.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct{
+	char mes[10];
+	int anyo; 
+}date;
+
+typedef struct{
+	char tipo[50];
+	float magnitud;
+	date fecha; 
+}generacion_2122;
+
+int max(int generacion_2122 generacion[][], int dim)
+{ 
+  
+  
+}
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
+#include "LibreriaBubalongas.h"
 
 //ESTRUCTURAS
 
